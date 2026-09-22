5c6b47d28328f3f84dae4d75ccd9efa92870f5d4
iirhash <iirhash08@gmail.com>
2026-09-22 13:51:11 +0800

    Update rtmp_server.js

--- rtmp_server.js
+++ rtmp_server.js
@@ -1,8 +1,8 @@
 const NodeMediaServer = require('node-media-server');
 
 const config = {
-    rtmp: { port: 1936, chunk_size: 1024, gop_cache: false, ping: 10, ping_timeout: 30 },
-    http: { port: 8000, allow_origin: '*' }
+  rtmp: { port: 1936, chunk_size: 1024, gop_cache: false, ping: 10, ping_timeout: 30 },
+  http: { port: 8000, allow_origin: '*' }
 };
 
 const nms = new NodeMediaServer(config);
@@ -12,7 +12,7 @@ console.log('==================================================');
 console.log('  RTMP Server running on port 1936');
 console.log('==================================================');
 console.log('  In DJI Mimo enter:');
-console.log('  rtmp://10.115.67.63:1936/live/stream');
+console.log('  rtmp://10.243.253.63:1936/live/stream');
 console.log('  Waiting for stream...');
 
 nms.on('prePublish', (id, StreamPath) => {
